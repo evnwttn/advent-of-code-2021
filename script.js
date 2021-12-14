7c328433d705e8e8bda2b5bab9f44f7f92290833
evnwttn <evnwttn@gmail.com>
2021-12-14 19:43:16 -0330

    WOOOOO :star:

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-function partOne() {
-  let oneCounter = 0;
-  for (let i = 0; i < input.length; i++) {
-    if (input[i].one === "1") {
-      oneCounter++;
-    }
-  }
-  console.log(oneCounter);
-}
+// function partOne() {
+//   let oneCounter = 0;
+//   for (let i = 0; i < input.length; i++) {
+//     if (input[i].twelve === "1") {
+//       oneCounter++;
+//     }
+//   }
+//   console.log(oneCounter);
+// }
 
-partOne();
+// partOne();
+
+gamma = 2663;
+epsilon = 1432;
+console.log(2663 * 1432);
+console.log("101001100111");
