80e50fa7f0fa3811a502ebbca7f818d4d29474d0
evnwttn <evnwttn@gmail.com>
2021-12-28 14:04:25 -0330

    returns incorrect, retiring this year

--- script.js
+++ script.js
@@ -16,6 +16,28 @@
 // console.log(2663 * 1432);
 // console.log("101001100111");
 
+// function partTwo() {
+//   let counter = 0;
+//   for (let i = 0; i < input.length; i++) {
+//     if (
+//       input[i].one === "1" &&
+//       input[i].two === "0" &&
+//       input[i].three === "0" &&
+//       input[i].four === "1" &&
+//       input[i].five === "1" &&
+//       input[i].six === "1" &&
+//       input[i].seven === "0" &&
+//       input[i].eight === "1" &&
+//       input[i].nine === "1" &&
+//       input[i].ten === "1" &&
+//       input[i].eleven === "1"
+//     ) {
+//       counter++;
+//     }
+//   }
+//   console.log(counter);
+// }
+
 function partTwo() {
   let counter = 0;
   for (let i = 0; i < input.length; i++) {
@@ -27,8 +49,7 @@ function partTwo() {
       input[i].five === "1" &&
       input[i].six === "0" &&
       input[i].seven === "1" &&
-      input[i].eight === "0" &&
-      input[i].nine === "0"
+      input[i].eight === "0"
     ) {
       counter++;
     }
@@ -36,11 +57,11 @@ function partTwo() {
   console.log(counter);
 }
 
-oxygen = 100111011110;
-oxy = 2526;
-c02 = 010010100;
-c0 = 148;
+let oxyBi = 10011101111;
+let oxy = 1263;
+let c02Bi = 01001010;
+let c02 = 74;
 
-console.log(oxy * c0);
+console.log(oxy * c02);
 
 partTwo();
